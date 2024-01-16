13503ec1ad9ccd4446080b348a89c46da32a91ab
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2024-01-16 20:17:53 -0300

    initial commit

---
+++ MyMod.cs
@@ -0,0 +1,19 @@
+﻿namespace OniModTemplate
+{
+    using System.Collections.Generic;
+    using HarmonyLib;
+    using KMod;
+
+    public class MyMod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+
+        public override void OnAllModsLoaded(Harmony harmony, IReadOnlyList<Mod> mods)
+        {
+            base.OnAllModsLoaded(harmony, mods);
+        }
+    }
+}
---
+++ run.ps1
@@ -0,0 +1,8 @@
+﻿Copy-Item "bin/Debug/$Env:modname-Merged.dll" -Destination "$Env:onisavefolder\mods\dev\$Env:modname" -force
+Copy-Item "mod.yaml" -Destination "$Env:onisavefolder\mods\dev\$Env:modname" -force
+Copy-Item "mod_info.yaml" -Destination "$Env:onisavefolder\mods\dev\$Env:modname" -force
+
+# Path should point to the steam url shortcut (the same one created by steam in your desktop)
+invoke-item "$Env:oniurlpath"
+
+Get-Content "$Env:onilogfolder\Player.log" -Wait
